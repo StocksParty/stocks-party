a98ffca7278f2fa341dc474a481302a507caa3b4
Nicholas <razvan.botorog@yahoo.com>
2024-10-21 23:37:32 -0400

    added necessarrily beans

---
+++ src/main/java/com/aws/project/config/AppConfig.java
@@ -0,0 +1,43 @@
+package com.aws.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.sns.SnsClient;
+
+/**
+ * Class for creating all the necessarily beans
+ */
+public class AppConfig {
+
+    /**
+     * RestTemplate bean
+     */
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    /**
+     * DynamoDB bean
+     */
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    /**
+     * SNS Client bean
+     */
+    @Bean
+    public SnsClient snsClient() {
+        return SnsClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
+    }
+}
--- src/test/java/com/aws/project/ProjectApplicationTests.java
+++ src/test/java/com/aws/project/ProjectApplicationTests.java
@@ -1,9 +1,12 @@
 package com.aws.project;
 
+import com.aws.project.config.AppConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(AppConfig.class)
 class ProjectApplicationTests {
 
 	@Test

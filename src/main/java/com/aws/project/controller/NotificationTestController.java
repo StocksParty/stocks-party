e19d647c468a932274035ee5814fd8e7e5e3dea3
Nicholas <razvan.botorog@yahoo.com>
2024-10-23 20:03:00 -0400

    feat(sns): notification controller test

--- src/main/java/com/aws/project/controller/NotificationTestController.java
+++ src/main/java/com/aws/project/controller/NotificationTestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class NotificationTestController {
 
-    private final StockPriceService stockPriceService;
     private final NotificationService notificationService;
 
     /**
